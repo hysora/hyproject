b4c5a9e437e589c8b2f177edc727fe30feb955cf
hysora <775431389@qq.com>
2017-05-02 23:43:35 +0800

    activemq

--- hyproject/src/main/java/com/study/activemq/queues/Producer.java
+++ hyproject/src/main/java/com/study/activemq/queues/Producer.java
@@ -5,6 +5,7 @@ import javax.jms.Destination;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,9 +17,11 @@ public class Producer{
 	public void sendMessage(Destination destination, final String message) {
 		jmsMessagingTemplate.convertAndSend(destination, message);
 	}
-
+	
+	@Async
 	@JmsListener(destination = "FirstQueue2")
 	public void consumerMessage(String text) {
-		System.out.println("从out.queue队列收到的回复报文为:" + text);
+		System.out.println("从FirstQueue2队列收到的回复报文为:" + text);
 	}
+	
 }
